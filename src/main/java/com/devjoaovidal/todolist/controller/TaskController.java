2157ba7b5762f8f853f9399573ab5b79715aab6a
João Vidal <dev.joaovidal@gmail.com>
2022-09-27 19:59:34 -0300

    almost finishing

--- src/main/java/com/devjoaovidal/todolist/controller/TaskController.java
+++ src/main/java/com/devjoaovidal/todolist/controller/TaskController.java
@@ -1,4 +1,57 @@
 package com.devjoaovidal.todolist.controller;
 
+import com.devjoaovidal.todolist.model.Task;
+import com.devjoaovidal.todolist.service.TaskService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@AllArgsConstructor
+@Slf4j
 public class TaskController {
+
+    private TaskService taskService;
+
+    @PostMapping("/tasks")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task createTask(@RequestBody Task task) {
+        log.info("Criando uma nova tarefa com as informações [{}]", task);
+        return taskService.createTask(task);
+    }
+
+    @GetMapping("/tasks")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Task> getAllTasks() {
+        log.info("Listando todas as tarefas cadastradas");
+        return taskService.listAllTasks();
+    }
+
+    @GetMapping("/tasks/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Task> getTaskById(@PathVariable (value = "id") Long id) {
+        log.info("Buscando tarefas com o id [{}]", id);
+        return taskService.findTaskById(id);
+    }
+
+    @PutMapping("/tasks/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Task> updateTaskById(@PathVariable (value = "id") Long id, @RequestBody Task task) {
+        log.info("Atualizando a tarefa com o id [{}] as novas infos são: [{}]", id, task);
+        return taskService.updateTaskById(task, id);
+    }
+
+
+    @DeleteMapping("/tasks/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object> deleteTaskById(@PathVariable (value = "id") Long id) {
+        log.info("Excluindo tarefa com o id: [{}]", id);
+        return taskService.deleteById(id);
+    }
 }
